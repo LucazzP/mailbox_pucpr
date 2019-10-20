f721310f57468e1107f7f7f73c93d652ca08187f
pdNN <pedrolucho02@gmail.com>
2019-10-20 14:23:13 -0300

    Menu toggle

--- js/mail.js
+++ js/mail.js
@@ -1,19 +1,25 @@
-$(document).ready(function() {
-    
-    $('#btn-new-email').click(function(e) {
-        e.preventDefault();
-        $.post(
-            "../php/mail.php", {
-                de: "lucas@gmail.com",
-                para: "renan@gmail.com",
-                cc: "pedrinho@gmail.com",
-                assunto: "O lincon eh viado",
-                texto: "eh mesmo"
-            },
-            function(data, textStatus, jqXHR) {
-                alert(data);
-            },
-        )
-    });
+$(document).ready(function () {
 
+   $('#btn-new-email').click(function (e) {
+      e.preventDefault();
+      $.post(
+         "../php/mail.php", {
+            de: "lucas@gmail.com",
+            para: "renan@gmail.com",
+            cc: "pedrinho@gmail.com",
+            assunto: "O lincon eh viado",
+            texto: "eh mesmo"
+         },
+         function (data, textStatus, jqXHR) {
+            alert(data);
+         },
+      )
+   });
+
+});
+
+$("#menu-toggle").click(function (e) {
+   e.preventDefault();
+   $("#wrapper").toggleClass("toggled");
+   $("#page-content-wrapper").toggleClass("col-12");
 });
