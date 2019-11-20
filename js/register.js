29e378608e631cba9cdd8e1cbcf08fe300c21522
Pedro Lucho <pedrolucho02@gmail.com>
2019-11-20 16:06:54 -0300

    favicon & login arrumado (falta email)

--- js/register.js
+++ js/register.js
@@ -1,46 +1,46 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
-   $("#btn-register").click(function (e) {
-      e.preventDefault();
-      var email = $("#email").val();
-      var senha = $("#password").val();
-      var senha2 = $("#password-again").val();
+  $("#btn-register").click(function(e) {
+    e.preventDefault();
+    var email = $("#email").val();
+    var senha = $("#password").val();
+    var senha2 = $("#password-again").val();
 
-      function testeEmail(email) {
-         var RegExp = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-         return RegExp.test(email);
-      };
-      if (testeEmail(email)) {
-         if (senha == senha2) {
-            $.post(
-               "http://" + window.location.host + "/php/register.php", {
-                  email: email,
-                  senha: senha2
-               },
-               function (data, textStatus, jqXHR) {
-                  //Login
-                  $.ajax({
-                     type: "POST",
-                     dataType: "json",
-                     url: "/php/login.php",
-                     data: {
-                         email: email,
-                         senha: senha
-                     },
-                     success: function(resposta) {
-                         if (resposta) {
-                             window.location.pathname = '/mail';
-                         }
-                     },
-                 });
-               },
-            )
-         } else {
-            alert("Senhas diferentes, digite novamente.");
-         }
+    function testeEmail(email) {
+      var RegExp = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+      return RegExp.test(email);
+    };
+    if (testeEmail(email)) {
+      if (senha == senha2) {
+        $.post(
+          window.location.origin + window.location.pathname.replace("/register/index.html", "/php/register.php"), {
+            email: email,
+            senha: senha2
+          },
+          function(data, textStatus, jqXHR) {
+            //Login
+            $.ajax({
+              type: "POST",
+              dataType: "json",
+              url: "../php/login.php",
+              data: {
+                email: email,
+                senha: senha
+              },
+              success: function(resposta) {
+                if (resposta) {
+                  window.location.href = '../mail';
+                }
+              },
+            });
+          },
+        )
       } else {
-         alert("E-mail inválido, digite novamente.");
-      };
+        alert("Senhas diferentes, digite novamente.");
+      }
+    } else {
+      alert("E-mail inválido, digite novamente.");
+    };
 
-   });
+  });
 });
